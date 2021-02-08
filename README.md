14b09a727e91bc98c57441a646e2497647e08343
Glitch (probable-fluorescent-celery) <none>
2021-02-08 18:12:30 +0000

    Add  correct code

--- README.md
+++ README.md
@@ -1,30 +1,55 @@
-# hello-express
+# Osinachi Chukwujama Take Home Documentation
+This is a web service which converts CSV to JSON and returns the required JSON fields. 
+It comes with this documentation and unit tests using Jest and supertest.
 
-A server that serves a webpage, its resources, and some data
+## Routes
+1. `POST /`
 
+### `POST /`
+#### Parameters
+1. csv: required (JSON object)
+    1. url: required (string)
+    2. select_fields: optional (string[])
 
-## Your Project
+#### Example request body
+  
+  ```json
+    {
+      "csv":{
+        "url": "https://linktocsv",
+        "select_fields": ["First Name", "Last Name", "Age"],
+      }
+    }
+  ```
+  
+#### Example response object
+    
+    ```json
+    {
+      "conversion_key": "ZEMAHBb54vkFXPHA9jHY6Xp3gMnMAKYg",
+      "json": [
+        {
+          "First Name":"Ade",
+          "Last Name":"Stark",
+          "Age": 21 
+        },
+        {
+          "First Name":"Ade",
+          "Last Name":"Stark",
+          "Age": 21 
+        }
+      ]
+    }
+    ```
+    
+## Running tests
+To run tests, run this command
+```bash
+npm test
+```
+## Test suites covered
+1. Testing the Post endpoint
 
-On the front-end,
-
-- Edit `views/index.html` to change the content of the webpage
-- `public/client.js` is the javacript that runs when you load the webpage
-- `public/style.css` is the styles for `views/index.html`
-- Drag in `assets`, like images or music, to add them to your project
-
-On the back-end,
-
-- your app starts at `server.js`
-- add frameworks and packages in `package.json`
-- safely store app secrets in `.env` (nobody can see this but you and people you invite)
-
-Click `Show` in the header to see your app live. Updates to your code will instantly deploy.
-
-
-## Made by [Glitch](https://glitch.com/)
-
-**Glitch** is the friendly community where you'll build the app of your dreams. Glitch lets you instantly create, remix, edit, and host an app, bot or site, and you can invite collaborators or helpers to simultaneously edit code with you.
-
-Find out more [about Glitch](https://glitch.com/about).
-
-( ᵔ ᴥ ᵔ )
+### Testing the Post endpoint
+1. Testing that it returns the correct status code
+2. Testing that the returned JSON is of the required schema
--- server.js
+++ server.js
@@ -1,34 +1,85 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const { v4: uuid } = require("uuid");
+const neatCsv = require('neat-csv');
+const bodyParser = require("body-parser");
 const express = require("express");
+const axios = require("axios");
+const detectCSV = require('detect-csv')
 const app = express();
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
+app.use(bodyParser.json());
 app.use(express.static("public"));
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+app.post("/", async (request, response) => {
+  response.contentType = "application/json"
+  if (Object.keys(request.body).length === 0) {
+    return response.status(400).send({ message: "CSV object is required" })
+  }
+  const {
+    csv: { url, select_fields }
+  } = request.body;
+  if (Object.keys(request.body).length === 0) {
+    return response.status(400).send({ message: "CSV object is required" })
+  }
+  if (!url) {
+    return response.status(400).send({ message: "url is required" })
+  }
+
+  try {
+    const { data: csvData } = await axios.get(url);
+    const csvMetaData = detectCSV(csvData)
+    if (!csvMetaData){
+      return response.status(400).send({ message: "csv is not valid" })
+    }
+    const {delimiter} = csvMetaData
+
+    const rawResults = await neatCsv(csvData, {separator: delimiter})
+    const allFields = Object.keys(rawResults[0])
+    let validFields = []
+
+    if (!select_fields || select_fields.length === 0) {
+      validFields = allFields
+    } else {
+      for (let field of select_fields) {
+        if (allFields.includes(field)) {
+          validFields.push(field)
+        }
+      }
+    }
+
+    const results = rawResults.map((row) => {
+      const validItems = {}
+      for (let field in row) {
+        if (validFields.includes(field)) {
+          validItems[field] = row[field]
+        }
+      }
+      return validItems
+    })
+
+    return response.json({
+      "conversion_key": uuid(),
+      json: results
+    });
+  } catch (error) {
+    if (error.errno === 'EPROTO') {
+      return response.status(400).json({
+        message: "Invalid URL"
+      })
+    }
+    
+    const { status, statusText } = error.response
+    return response.status(status).json({
+      message: statusText
+    })
+  }
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
+app.get('/', (request, response) => {
+  response.sendFile(__dirname + "/public/index.html");
+})
 
-// listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+module.exports = app
